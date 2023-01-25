cc69805fe0e37512a54566da72725d74ca250106
Ezekiel <ezekielchow94@gmail.com>
2023-01-26 00:26:23 +0800

    fix mongoose connection

--- tests/setup.js
+++ tests/setup.js
@@ -3,41 +3,27 @@ const { mongoose } = require('mongoose');
 
 require('dotenv').config({ path: '.env.test' });
 
-describe('MongodbMemoryServer init', () => {
-  let mongod;
-  const dbName = 'stashawayTestDb';
+let mongod;
+const dbName = 'stashawayTestDb';
 
-  beforeAll(async () => {
-    // mongod = await MongoMemoryServer.create();
+beforeAll(async () => {
+  mongod = await MongoMemoryServer.create();
 
-    // const mongoUri = mongod.getUri();
+  const mongoUri = mongod.getUri();
 
-    // await mongoose.connect(mongoUri, {
-    //   dbName,
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
+  await mongoose.connect(mongoUri, {
+    dbName,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
+});
 
-  beforeEach(async () => {
-    // const collections = await mongoose.connection.db.collections();
+afterAll(async () => {
+  if (mongoose.connection) {
+    await mongoose.connection.close();
+  }
 
-    // collections.forEach(async (collection) => {
-    //   await collection.deleteMany();
-    // });
-  });
-
-  afterAll(async () => {
-    // if (mongod) {
-    //   await mongod.stop();
-    // }
-
-    // if (mongoose.connection) {
-    //   await mongoose.connection.close();
-    // }
-  });
-
-  it('should connection successful', async () => {
-    // expect(mongoose.connection.readyState).toBe(1);
-  });
+  if (mongod) {
+    await mongod.stop();
+  }
 });
