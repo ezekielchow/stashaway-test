c8ef83f21e2d36e5f3e379714b0a992f5575339e
Ezekiel <ezekielchow94@gmail.com>
2023-01-28 22:50:58 +0800

    remove default import

--- services/user-deposit-service.ts
+++ services/user-deposit-service.ts
@@ -1,6 +1,6 @@
 import { DepositPlan, TYPES } from '../models/deposit-plan';
 import { UserDeposit } from '../models/user-deposit';
-import UserDepositDistribution from '../models/user-deposit-distribution';
+import { UserDepositDistribution } from '../models/user-deposit-distribution';
 
 import { IDepositPlan } from "../models/deposit-plan";
 import { IUser } from "../models/user";
